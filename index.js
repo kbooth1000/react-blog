9caffb6740c1aac01cfe608d3f4f5b7a27e7cd12
Kyle Booth <kbooth1000@gmail.com>
2018-04-17 10:45:47 -0400

    Code thread irretrievably tangled

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 const h = React.createElement;
 const root = document.querySelector('.react-root');
 
-let posts = [{
-        id: '1',
-        title: 'My first blog post',
-        author: 'Catfish McGee',
-        imagepath: 'img/cmcgee.png',
-        body: 'Well, let me tell you a little story. It all began...'
-    },
-
-    {
-        id: '2',
-        title: 'Sweet Potato Pie',
-        author: 'Calpernia',
-        image: 'img/calpernia.png',
-        body: 'One of my favorite cold-weather dishes is sweet potato ...'
-    }
-];
 
 let postBeingEdited = null;
 
@@ -37,7 +21,7 @@ let Footer = ({
 let Title = ({
         title
     }) =>
-    h('h1', {
+        h('h1', {
         className: 'title'
     }, title);
 
@@ -61,88 +45,109 @@ let Image = ({
     h('img', {
         src: `${imagepath}`
     });
+//  #################################
 
-let Post = props => h('section', null, [
-    h(Title, props),
-    h(Image, props),
-    h(Body, props),
-    h(Author, props),
-    h(DeletePostButton, props),
-    h(EditPostButton, props)
-]);
-let deletePost = function (id) {
-    posts = posts.filter((post) => post.id !== id);
-    console.log('posts: ', posts);
-}
+
+
+
 let DeletePostButton = ({
-        id
+        id, deletePost
     }) =>
     h('button', {
         onClick: () => deletePost(id)
     }, 'Delete');
 
-let updatePost = function (props) {
-    console.log('props.body, value: ', props.body);
+let updatePost = function (post, event) {
+    console.log('post.body, value: ', post.body);
     postToEdit = posts.find(post => post.id === props.id);
     postToEdit.body = props.body;
     postBeingEdited = null;
 }
 
-let updatePostBody = function (props, value) {
-    let post = posts.find(post => post.id === props.id);
-    console.log('post: ', post.body, value);
-    post.body = value;
-}
+// let updatePostBody = function ({post}, {posts}, value) {
+//      let blogpost = posts.find(blogpost => blogpost.id === post.id);
+//     console.log('post: ', post.body, value, (post === blogpost) );
+//     post.body = value;
+// }
 
-let EditSubmitButton = props => h('button', {
+let EditSubmitButton = post => h('button', {
     value: 'Submit',
-    onClick: (event) => updatePost(props)
+    onClick: (event) => updatePost(post, event)
 }, 'Save');
 
-let PostEditField = props => h('textarea', {
+let PostEditField = ({post, posts, updatePostBody, postBeingEdited}) => h('textarea', {
     rows: '10',
     cols: '30',
-    value: props.body,
-    onChange: (event) => updatePostBody(props, event.target.value)
+    value: post.body //,
+    // onChange: (event) => updatePostBody({post}, {postBeingEdited}, {posts}, event.target.value)
 });
 
+let EditPostButton = ({editPost, post}) => { 
+return h('button', {
+    onClick: () => editPost(post)
+}, 'Edit');}
 
-
-let EditPostButton = props => h('button', {
-    onClick: () => editPost(props)
-}, 'Edit');
-
-let PostEditForm = (props) => {
-    console.log('PostEditForm: ', props.body);
+let PostEditForm = ({post, posts, postBeingEdited, editPost, updatePostBody}) => {
+    console.log('PostEditForm: ', post.body);
     return h('form', {}, [
-        h(PostEditField, props),
-        h(EditSubmitButton, props)
+        h(PostEditField, {post, posts, updatePostBody, postBeingEdited}),
+        h(EditSubmitButton, {post})
     ]);
 }
 
-let PostList = props =>
-    h('div', null, props.posts.map((post) => {
-        if (props.postBeingEdited && props.post.id === props.postBeingEdited.id) {
-            return h(PostEditForm, post);
+let Post = (post, posts, postBeingEdited, editPost, deletePost) =>{ 
+    console.log('post, posts, postBeingEdited, editPost: ',post, posts, postBeingEdited, editPost);
+return h('section', null, [
+    h(Title, post),
+    h(Image, post),
+    h(Body, post),
+    h(Author, post),
+    h(DeletePostButton, post, deletePost),
+    h(EditPostButton, {editPost, post})
+]);}
+
+let PostList = ({posts, postBeingEdited, editPost, deletePost, updatePostBody }) => {
+    return h('div', null, posts.map((post) => {
+        if (postBeingEdited && post.id === postBeingEdited.id) {
+            return h(PostEditForm, {post, posts, postBeingEdited, editPost, updatePostBody});
         } else {
-            return h(Post, post);
+            return h(Post,  {
+                post, posts, postBeingEdited, editPost, deletePost
+            });
         }
     }));
+}
 
+
+
+
+/// Page class   ######
 class Page extends React.Component {
-    constructor(props) {
-        super(props);
+    constructor(allPosts) {
+        super(allPosts);
         this.state = {
             postBeingEdited: null,
-            posts: posts
+            posts: allPosts
         };
     }
     
     render() {
-        let {posts, postBeingEdited } = this.state;
+        // let {posts, postBeingEdited } = this.state;
+
+        let updatePostBody = ({post}, {postBeingEdited}, {posts}, value) => {
+            this.setState({
+                postBeingEdited: Object.assign({}, post, value)
+            });
+        };
+
+        let deletePost = function (id) {
+            this.state.posts.setState( (post) => post.id === id);
+            console.log('posts: ', posts);
+        }
+
         let editPost = (post)=>{
             console.log('Edit this: ', post.body);
-            this.state.postBeingEdited = post;
+            this.setState({postBeingEdited : post});
         }
 
         return h('div', {
@@ -151,7 +156,13 @@ class Page extends React.Component {
             h(Greeting, {
                 name: 'Louise'
             }),
-            h(PostList, { posts, postBeingEdited }),
+            h(PostList, { 
+                posts:this.state.posts.allPosts, 
+                postBeingEdited:this.state.postBeingEdited,
+                editPost:editPost,
+                deletePost:deletePost,
+                updatePostBody:updatePostBody
+            }),
             h(Footer, {
                 copyright: '2018'
             })
@@ -160,5 +171,20 @@ class Page extends React.Component {
 }
 
 ReactDOM.render(
-    h(Page), root
+    h(Page, { allPosts:[{
+        id: '1',
+        title: 'My first blog post',
+        author: 'Catfish McGee',
+        imagepath: 'img/cmcgee.png',
+        body: 'Well, let me tell you a little story. It all began...'
+    },
+
+    {
+        id: '2',
+        title: 'Sweet Potato Pie',
+        author: 'Calpernia',
+        image: 'img/calpernia.png',
+        body: 'One of my favorite cold-weather dishes is sweet potato ...'
+    }
+] }), root
 );
